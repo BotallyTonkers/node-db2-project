b989208ff3da7905efcc8e05ee76d246a092d950
Amanda Young <botallytonkers@gmail.com>
2023-11-15 13:50:10 -0700

    12

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 const express = require("express")
+const carsRouter = require('./cars/cars-router')
 
 const server = express()
 
 server.use(express.json())
 
+server.use('/api/cars', carsRouter)
+
 server.use('*', (req, res, next) => {
-    next({ status: 404, message: 'not found' })
+    next({ status: 404, message: 'not found!' })
 })
 
 server.use((err, req, res, next) => { //eslint-disable-line
@@ -13,6 +16,5 @@ server.use((err, req, res, next) => { //eslint-disable-line
         message: err.message
     })
 })
-//
 
 module.exports = server
